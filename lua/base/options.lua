29493a96e7fecd5232481bb133849bb56f02bccd
karthik <karthik@KRATOS.localdomain>
2023-12-27 22:28:53 +0530

    Setting more vim options through options.lua

--- lua/base/options.lua
+++ lua/base/options.lua
@@ -2,6 +2,14 @@ local o = vim.opt
 
 o.number = true
 o.relativenumber = true
+o.list = true
+o.listchars = { tab = "→ ", trail = "·", extends = "»", precedes = "«", eol = "¬" }
+
+o.background = "dark"
+o.termguicolors = true
+o.cursorline = true
+
+o.signcolumn = "yes"
 
 o.expandtab = true
 o.tabstop = 2
@@ -9,4 +17,26 @@ o.softtabstop = 2
 o.shiftwidth = 2
 o.shiftround = true
 
+o.ignorecase = true
+o.hlsearch = false
+o.smartcase = true
+
+o.smartindent = true
+
+o.scrolloff = 15
+o.sidescrolloff = 5
+
+o.splitright = true
+o.splitbelow = true
+o.swapfile = false
+o.undofile = true
+o.virtualedit = "block"
+o.mouse = "a"
+o.mousemoveevent = true
+
+o.updatetime = 250
 o.timeoutlen = 300
+
+o.clipboard:append("unnamedplus")
+o.shortmess:append("IsF")
+
