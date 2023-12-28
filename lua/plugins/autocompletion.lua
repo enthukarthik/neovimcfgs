221435a5992a2ead11bbb347ecb95829d0a35615
karthik <karthik@KRATOS.localdomain>
2023-12-29 03:30:43 +0530

    Removing autocompletion. Have to revist

--- lua/plugins/autocompletion.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "hrsh7th/nvim-cmp", event = "VeryLazy", config = require("plugins.overrides.autocompletion").cmpsetup,  },
-  { "hrsh7th/cmp-buffer", event = "VeryLazy", },
-  { "hrsh7th/cmp-path", event = "VeryLazy", },
-  { "hrsh7th/cmp-cmdline", event = "VeryLazy", },
-  { "saadparwaiz1/cmp_luasnip", event = "VeryLazy", },
-}
