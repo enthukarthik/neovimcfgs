edcae76452b7681c5ad00fef44d08a23ba453102
karthik <karthik@KRATOS.localdomain>
2024-01-01 04:17:32 +0530

    Setting up lspconfig

--- lua/plugins/nvimessentials.lua
+++ lua/plugins/nvimessentials.lua
@@ -31,35 +31,4 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = require("plugins.overrides.lualine").lualinesetup,
   },
-  {
-    -- gcc - to add a linewise comment
-    -- gbc - to add blockwise comment
-    -- gc - same as gcc in visual mode
-    -- gb - same as gbc in visual mode
-    -- gcO - add line comment above and enter Insert mode
-    -- gco - add line comment below and enter Insert mode
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
-    opts = {},
-  },
-  {
-    -- Usage :
-    -- ys{motion}{char} - add
-    -- ds{char} - delete
-    -- cs{target}{replacement} - change
-    --     Old text                    Command         New text
-    -- --------------------------------------------------------------------------------
-    --     surr*ound_words             ysw)           (surround_words)
-    --     *make strings               ys$"            "make strings"
-    --     [delete ar*ound me!]        ds]             delete around me!
-    --     remove <b>HTML t*ags</b>    dst             remove HTML tags
-    --     delete(functi*on calls)     dsf             function calls
-    --     'change quot*es'            cs'"            "change quotes"
-    --     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
-    --     Detailed information on how to use this plugin can be found in :h nvim-surround.usage
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "InsertEnter",
-    opts = {},
-  },
 }
--- lua/plugins/overrides/neotree.lua
+++ lua/plugins/overrides/neotree.lua
@@ -3,6 +3,7 @@ local M = {}
 M.neotreekeymap = {
   { "<C-n>", "<cmd>Neotree float toggle<CR>" },
   { "<leader>fr", "<cmd>Neotree float reveal<CR>" },
+  { "<leader>nt", "<cmd>Neotree right toggle<CR>" },
 }
 
 M.neotreesetup = function()
