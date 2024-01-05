8d959686a02585b067cffde99fa7acb398c7dbfc
karthik <karthik@KRATOS.localdomain>
2024-01-05 17:21:30 +0530

    Avoid checkhealth warnings and report only auto session errors, if any

--- lua/plugins/nvimessentials.lua
+++ lua/plugins/nvimessentials.lua
@@ -134,7 +134,7 @@ return {
       vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
       require("auto-session").setup({
-        log_level = "info",
+        log_level = "error",
         auto_save_enabled = true,
         auto_restore_enabled = true,
         auto_session_use_git_branch = true,
